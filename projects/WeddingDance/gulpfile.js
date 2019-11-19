e842d8c812b965e88a083de6d05567d67bcddc39
Lukachi <lukachisama@gmail.com>
2019-11-19 15:16:06 +0200

    added work Code
    Added work Code

---
+++ projects/WeddingDance/gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const server = require('browser-sync').create();
+
+function style() {
+    return gulp.src('./scss/**/*.scss')
+        .pipe(sass())
+        .pipe(autoprefixer('last 5 versions'))
+        .pipe(gulp.dest('./assets/styles/'))
+        .pipe(server.stream());
+}
+
+function vendors() {
+    return gulp.src(['node_modules/jquery/dist/jquery.min.js', 'node_modules/slick-carousel/slick/slick.min.js'])
+        .pipe(concat('vendors.js'))
+        .pipe(gulp.dest('./assets/vendors/'));
+}
+
+
+function defaultTask() {
+    vendors();
+    server.init({
+        server: './'
+    });
+    gulp.watch('./scss/**/*.scss', style);
+    gulp.watch('./assets/scripts/**/*.js').on('change', server.reload);
+    gulp.watch('./*.html').on('change', server.reload);
+}
+
+gulp.task('default', defaultTask);
+
+gulp.task('vendors', vendors);
