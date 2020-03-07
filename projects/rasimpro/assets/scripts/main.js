884ea22496cdf79a5b81461ec5f9b26300d13606
Lukachi <lukachisama@gmail.com>
2020-03-07 19:30:43 +0200

    responsive fixes and video extracting

--- projects/rasimpro/assets/scripts/main.js
+++ projects/rasimpro/assets/scripts/main.js
@@ -1,13 +1,163 @@
+// ——————————————————————————————————————————————————
+// TextScramble
+// ——————————————————————————————————————————————————
+
+class TextScramble {
+    constructor(el) {
+        this.el = el;
+        this.chars = '!<>-_\\/[]{}—=+*^?#________';
+        this.update = this.update.bind(this);
+    }
+
+    setText(newText) {
+        const oldText = this.el.innerText;
+        const length = Math.max(oldText.length, newText.length);
+        const promise = new Promise((resolve) => this.resolve = resolve);
+        this.queue = [];
+        for (let i = 0; i < length; i++) {
+            const from = oldText[i] || '';
+            const to = newText[i] || '';
+            const start = Math.floor(Math.random() * 40);
+            const end = start + Math.floor(Math.random() * 40);
+            this.queue.push({from, to, start, end});
+        }
+        cancelAnimationFrame(this.frameRequest);
+        this.frame = 0;
+        this.update();
+        return promise;
+    }
+
+    update() {
+        let output = '';
+        let complete = 0;
+        for (let i = 0, n = this.queue.length; i < n; i++) {
+            let {from, to, start, end, char} = this.queue[i];
+            if (this.frame >= end) {
+                complete++;
+                output += to;
+            } else if (this.frame >= start) {
+                if (!char || Math.random() < 0.28) {
+                    char = this.randomChar();
+                    this.queue[i].char = char;
+                }
+                output += `<span class="dud">${char}</span>`;
+            } else {
+                output += from;
+            }
+        }
+        this.el.innerHTML = output;
+        if (complete === this.queue.length) {
+            this.resolve();
+        } else {
+            this.frameRequest = requestAnimationFrame(this.update);
+            this.frame++;
+        }
+    }
+
+    randomChar() {
+        return this.chars[Math.floor(Math.random() * this.chars.length)];
+    }
+}
+
+function ScramblingElement(DOMElement) {
+    const phrases = [
+        ...DOMElement.attr("data-textInsteads").split(",")
+    ];
+
+    const el = DOMElement[0];
+    const fx = new TextScramble(el);
+
+    let counter = 0;
+    const next = () => {
+        fx.setText(phrases[counter]).then(() => {
+            setTimeout(next, 5000);
+        });
+        counter = (counter + 1) % phrases.length
+    };
+
+    next();
+}
+
+function fadeRightTextLetterByLetter(DOMElement, duration) {
+    var textWrapper = DOMElement;
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+    anime.timeline({loop: false})
+        .add({
+            targets: textWrapper,
+            opacity: [0, 1],
+            easing: "easeInOutQuad",
+            duration: duration,
+            delay: (el, i) => 25 * (i + 1)
+        });
+}
+
+function portfolioSlidering(goAllPortfolioBtn_element_center_original) {
+    // отсчёт от начала окна + центр экрана
+    let centerScreen = $([document.documentElement, document.body]).scrollTop() + ($(window).height() / 2);
+
+    // конец секции с портфолио
+    let wrapper_endpoints_Y = $(".portfolio__wrapper").offset().top + $(".portfolio__wrapper").height();
+
+    if (centerScreen >= goAllPortfolioBtn_element_center_original.y &&
+        centerScreen <= wrapper_endpoints_Y - ($(".portfolio__wrapper .portfolio-item").height() / 2)) {
+        if (!$(".go-all-portfolio").hasClass("fixing")) {
+            $(".go-all-portfolio").addClass("fixing");
+        }
+    } else {
+        if (centerScreen <= goAllPortfolioBtn_element_center_original.y) {
+            //over
+            if ($(".go-all-portfolio").hasClass("fixing")) {
+                $(".go-all-portfolio").removeClass("fixing");
+            }
+            if ($(".go-all-portfolio-wrapper").hasClass("end")) {
+                $(".go-all-portfolio-wrapper").removeClass("end");
+            }
+        } else if (centerScreen >= wrapper_endpoints_Y - ($(".portfolio__wrapper .portfolio-item").height() / 2)) {
+            //under
+            if ($(".go-all-portfolio").hasClass("fixing")) {
+                $(".go-all-portfolio").removeClass("fixing");
+            }
+            if (!$(".go-all-portfolio_wrapper").hasClass("end")) {
+                $(".go-all-portfolio-wrapper").addClass("end");
+            }
+        }
+    }
+}
+
+function toggleMenu() {
+    const menu_wrapper = $(".header-menu__wrapper");
+    menu_wrapper.css({
+        "transition": "none",
+        "height": window.innerHeight
+    });
+
+    menu_wrapper.toggleClass("show");
+    $(".header-menu__wrapper .header-menu__burger.close").toggleClass("show");
+}
+
+
 $(document).ready(function () {
+
+    // header-menu
+    $(".header-menu__label").on("click", function (e) {
+        toggleMenu();
+    });
+
+    // footer-menu
+    $(".footer-menu__label").on("click", function (e) {
+        toggleMenu();
+    });
+
     // Инициализация анимаций для плагина AOS
     AOS.init();
 
-    $(window).on("load", function () {
-        AOS.refresh();
-    });
-
     // ресайз элементов при деформации окна
     $(window).on("resize", function (e) {
+
+        $(".header-menu__wrapper").css({
+            "height": window.innerHeight
+        });
         if ($(".reviews-slider").length) {
             $(".reviews-slider").slick('resize');
         }
@@ -31,6 +181,15 @@ $(document).ready(function () {
         portfolioSlidering(goAllPortfolioBtn_element_center_original);
 
 
+        if ($(".header-menu__wrapper").hasClass("show")) {
+            $(window).scrollTop(0);
+
+            const menu_wrapper = $(".header-menu__wrapper");
+            menu_wrapper.css({
+                "transition": "none",
+                "height": window.innerHeight
+            });
+        }
     });
 
     // parallax effect on elements
@@ -77,19 +236,6 @@ $(document).ready(function () {
         $([document.documentElement, document.body]).animate({
             scrollTop: $("header.header").offset().top
         }, 1000);
-    });
-
-    // header-menu
-    $(".header-menu__label").on("click", function (e) {
-        $(".header-menu__wrapper").toggleClass("show");
-        $(".header-menu__wrapper .header-menu__burger.close").toggleClass("show");
-        $(".main-content__article").toggleClass("cropped");
-    });
-    // footer-menu
-    $(".footer-menu__label").on("click", function (e) {
-        $(".header-menu__wrapper").toggleClass("show");
-        $(".header-menu__wrapper .header-menu__burger.close").toggleClass("show");
-        $(".main-content__article").toggleClass("cropped");
     });
 
     // sliders
@@ -189,127 +335,3 @@ $(document).ready(function () {
     // });
 
 });
-
-// ——————————————————————————————————————————————————
-// TextScramble
-// ——————————————————————————————————————————————————
-
-class TextScramble {
-    constructor(el) {
-        this.el = el;
-        this.chars = '!<>-_\\/[]{}—=+*^?#________';
-        this.update = this.update.bind(this);
-    }
-    setText(newText) {
-        const oldText = this.el.innerText;
-        const length = Math.max(oldText.length, newText.length);
-        const promise = new Promise((resolve) => this.resolve = resolve);
-        this.queue = [];
-        for (let i = 0; i < length; i++) {
-            const from = oldText[i] || '';
-            const to = newText[i] || '';
-            const start = Math.floor(Math.random() * 40);
-            const end = start + Math.floor(Math.random() * 40);
-            this.queue.push({ from, to, start, end });
-        }
-        cancelAnimationFrame(this.frameRequest);
-        this.frame = 0;
-        this.update();
-        return promise;
-    }
-    update() {
-        let output = '';
-        let complete = 0;
-        for (let i = 0, n = this.queue.length; i < n; i++) {
-            let { from, to, start, end, char } = this.queue[i];
-            if (this.frame >= end) {
-                complete++;
-                output += to;
-            } else if (this.frame >= start) {
-                if (!char || Math.random() < 0.28) {
-                    char = this.randomChar();
-                    this.queue[i].char = char;
-                }
-                output += `<span class="dud">${char}</span>`;
-            } else {
-                output += from;
-            }
-        }
-        this.el.innerHTML = output;
-        if (complete === this.queue.length) {
-            this.resolve();
-        } else {
-            this.frameRequest = requestAnimationFrame(this.update);
-            this.frame++;
-        }
-    }
-    randomChar() {
-        return this.chars[Math.floor(Math.random() * this.chars.length)];
-    }
-}
-
-function ScramblingElement(DOMElement) {
-    const phrases = [
-        ...DOMElement.attr("data-textInsteads").split(",")
-    ];
-
-    const el = DOMElement[0];
-    const fx = new TextScramble(el);
-
-    let counter = 0;
-    const next = () => {
-        fx.setText(phrases[counter]).then(() => {
-            setTimeout(next, 5000);
-        });
-        counter = (counter + 1) % phrases.length
-    };
-
-    next();
-}
-
-function fadeRightTextLetterByLetter(DOMElement, duration) {
-    var textWrapper = DOMElement;
-    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-    anime.timeline({loop: false})
-        .add({
-            targets: textWrapper,
-            opacity: [0,1],
-            easing: "easeInOutQuad",
-            duration: duration,
-            delay: (el, i) => 25 * (i+1)
-        });
-}
-
-function portfolioSlidering(goAllPortfolioBtn_element_center_original) {
-    // отсчёт от начала окна + центр экрана
-    let centerScreen = $([document.documentElement, document.body]).scrollTop() + ($(window).height() / 2);
-
-    // конец секции с портфолио
-    let wrapper_endpoints_Y = $(".portfolio__wrapper").offset().top + $(".portfolio__wrapper").height();
-
-    if (centerScreen >= goAllPortfolioBtn_element_center_original.y &&
-        centerScreen <= wrapper_endpoints_Y - ($(".portfolio__wrapper .portfolio-item").height() / 2)) {
-        if (!$(".go-all-portfolio").hasClass("fixing")) {
-            $(".go-all-portfolio").addClass("fixing");
-        }
-    } else {
-        if (centerScreen <= goAllPortfolioBtn_element_center_original.y) {
-            //over
-            if ($(".go-all-portfolio").hasClass("fixing")) {
-                $(".go-all-portfolio").removeClass("fixing");
-            }
-            if ($(".go-all-portfolio-wrapper").hasClass("end")) {
-                $(".go-all-portfolio-wrapper").removeClass("end");
-            }
-        } else if (centerScreen >= wrapper_endpoints_Y - ($(".portfolio__wrapper .portfolio-item").height() / 2)) {
-            //under
-            if ($(".go-all-portfolio").hasClass("fixing")) {
-                $(".go-all-portfolio").removeClass("fixing");
-            }
-            if (!$(".go-all-portfolio_wrapper").hasClass("end")) {
-                $(".go-all-portfolio-wrapper").addClass("end");
-            }
-        }
-    }
-}
