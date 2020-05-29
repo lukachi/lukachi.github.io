7e6b9d3e5f7d1703f53556ca8987634fbbe7c996
Lukachi <lukachisama@gmail.com>
2020-05-29 16:54:16 +0300

    7-secrets(Desktop)

---
+++ projects/7-secrets/scripts/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    const ISTOUCH = 'ontouchstart' in window || navigator.msMaxTouchPoints;
+    if (!ISTOUCH) {
+        $(".reviews__item .item-container").tilt({
+            maxTilt: 5,
+            scale: 1.05
+        });
+        $(".gifts__item").tilt({
+            maxTilt: 1.5,
+            scale: 1.05,
+            axis: 'x'
+        });
+        $(".nuances-wrapper .img-wrapper img").tilt({
+            maxTilt: 3,
+            scale: 1.05,
+        });
+    }
+});
