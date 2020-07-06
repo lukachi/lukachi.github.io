3a21cc3acac06ac068d0a5dbab31b15d7a845d9b
Lukachi <lukachisama@gmail.com>
2020-07-06 15:16:11 +0300

    full(without animations)

--- projects/lara-german/assets/scripts/main.js
+++ projects/lara-german/assets/scripts/main.js
@@ -9,9 +9,43 @@ $(document).ready(function () {
   $(".plan__nav .arrow-right").on("click", function (e) {
     $(".plan__wrapper").slick("slickNext");
   });
+  $(".plan__wrapper").on('afterChange', function () {
+    let index = $('.slick-current').attr("data-slick-index");
+    const slider_length = $(".slick-dots li").length;
+
+    if (index == 0) {
+      $(".plan__nav .arrow-left").removeClass("has");
+      $(".plan__nav .arrow-right").addClass("has");
+    } else if (index > 0 && index < slider_length - 1) {
+      $(".plan__nav .arrow-left").addClass("has");
+      $(".plan__nav .arrow-right").addClass("has");
+    } else if (index == slider_length - 1) {
+      $(".plan__nav .arrow-left").addClass("has");
+      $(".plan__nav .arrow-right").removeClass("has");
+    }
+
+    if ($(".plan__wrapper .slick-dots").hasClass("hide")) {
+      $(".plan__wrapper .slick-dots").toggleClass("hide");
+    }
+  });
   $(".plan__item .toggle").on("click", function (e) {
     $(this).parent().toggleClass("show");
     $(".plan__wrapper .slick-dots").toggleClass("hide");
+
+    if ($(this).parent().hasClass("show")) {
+      $(this).text("Свернуть");
+    } else {
+      $(this).text("Прочитать полностью");
+    }
+  });
+  $(".faq__header").on("click", function (e) {
+    $(this).next().slideToggle();
+    $(this).parent().toggleClass("show");
+  });
+  $(".btn-top").click(function () {
+    $([document.documentElement, document.body]).animate({
+      scrollTop: 0
+    }, 1000);
   });
   AOS.init({
     // Global settings:
@@ -36,9 +70,5 @@ $(document).ready(function () {
     once: true // whether animation should happen only once - while scrolling down
     // mirror: false, // whether elements should animate out while scrolling past them
 
-  });
-  $(".faq__header").on("click", function (e) {
-    $(this).next().slideToggle();
-    $(this).parent().toggleClass("show");
   });
 });
